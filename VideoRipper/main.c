ae43a18493cb4349ce6e2c149e56d6331f966e1e
Larson Carter <larsoncarter72404@gmail.com>
2024-04-17 13:05:43 -0500

    Initial Commit

---
+++ VideoRipper/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  VideoRipper
+//
+//  Created by Larson Carter on 4/17/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
